3799d08eee77f9c87e8f3087e38ea46fe06fa461
gabrielbarreto <gabriel.devloper@gmail.com>
2024-07-18 21:00:21 -0300

    feat: cabecalho da aplicacao finalizado.

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,9 +1,9 @@
-import Header from '@/componets/header'
+import { Header } from '@/components/header'
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
       <Header />
       {children}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     },
 
     gridTemplateRows: {
-      appp: 'min-content max-content',
+      app: 'min-content max-content',
     },
   },
   plugins: [],
