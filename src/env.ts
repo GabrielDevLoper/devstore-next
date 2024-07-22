8af057ea1543535483c231bc10c8d75252d2eb88
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-07-22 19:53:43 -0300

    feat: buscando dados da api interna no nextjs

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,18 @@
-import { parse } from 'path'
 import { z } from 'zod'
 
 const envSchema = z.object({
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
 
 if (!parsedEnv.success) {
-    console.log(
-        'invalid environment variables.',
-        parsedEnv.error.flatten().fieldErrors,
-    )
+  console.log(
+    'invalid environment variables.',
+    parsedEnv.error.flatten().fieldErrors,
+  )
 
-    throw new Error('invalid environment variables.')
+  throw new Error('invalid environment variables.')
 }
 
 export const env = parsedEnv.data
